7ffa5551cb6accc0f36435449381f641fdf929c2
FloreDenis2001 <floredenis907@yahoo.com>
2024-06-10 16:27:51 +0300

    last_update

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,5 +1,9 @@
 "use client"
-import React, { useState } from 'react';
+import React from 'react';
+import emailjs from 'emailjs-com';
+import { useState } from 'react';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 export default function Contact() {
